455d9a08428d7ef7604bb155b2dcf0204b2c7034
Paulo Gabriel <pgj1085@gmail.com>
2020-06-23 15:23:58 -0300

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -30,7 +30,7 @@ export class PickAddressPage {
     this.pedido.enderecoDeEntrega = {
       id: endereco.id
     };
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', { pedido: this.pedido });
   }
 
   ionViewDidLoad(): void {
