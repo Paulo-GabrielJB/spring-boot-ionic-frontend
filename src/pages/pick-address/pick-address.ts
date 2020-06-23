7eae096d6abbec1c2bb217ce9f88b638fb08b4a5
Paulo Gabriel <pgj1085@gmail.com>
2020-06-23 14:59:51 -0300

    Armazenando dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -4,6 +4,9 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
 import { LocalUser } from '../../models/local_user';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/cart.service';
+import { Cart } from '../../models/cart';
 
 @IonicPage()
 @Component({
@@ -14,19 +17,44 @@ export class PickAddressPage {
 
 
   items: EnderecoDTO[];
+  pedido: PedidoDTO;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public clienteService: ClienteService,
-    public storageService: StorageService) {
+    public storageService: StorageService,
+    public cartService: CartService) {
   }
 
-  ionViewDidLoad() {
+  nextPage(endereco: EnderecoDTO): void{
+    this.pedido.enderecoDeEntrega = {
+      id: endereco.id
+    };
+    console.log(this.pedido);
+  }
+
+  ionViewDidLoad(): void {
     let localUser: LocalUser = this.storageService.getLocalUser();
     if(localUser && localUser.email)
       this.clienteService.findByEmail(localUser.email).subscribe(
         response => {
           this.items = response.enderecos;
+
+          let cart: Cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: { id: response.id },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map(x => {
+              return {
+                quantidade: x.quantidade,
+                produto: {
+                  id: x.produto.id
+                }
+              }
+            })
+          }
         }, 
         error => {
           if(error.status == 403)
